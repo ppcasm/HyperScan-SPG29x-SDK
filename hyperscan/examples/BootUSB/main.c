ef80714c2b064a3ac07e51afa95e98908767d28f
Jonathan Benjamin <ppcasm@gmail.com>
2024-03-12 07:37:36 -0400

    Update USB booting, fixes, etc

--- hyperscan/examples/BootUSB/main.c
+++ hyperscan/examples/BootUSB/main.c
@@ -17,6 +17,7 @@ DO NOT TRY TO RUN THIS AS A HYPER.EXE APP; IT WILL NOT WORK...
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "./FS/ff.h"
 #include "./FS/diskio.h"
@@ -24,32 +25,110 @@ DO NOT TRY TO RUN THIS AS A HYPER.EXE APP; IT WILL NOT WORK...
 #include "../../include/SPG290_Registers.h"
 #include "../../include/SPG290_Constants.h"
 
-#define USB_LOAD() ((void (*)(void))0xA0091000)()
+#define MAX_LINE_LENGTH 64
+#define MAX_KEY_LENGTH 24
+#define MAX_VALUE_LENGTH 24
+#define MAX_ENTRIES 4
+
+typedef struct {
+    char key[MAX_KEY_LENGTH];
+    char value[MAX_VALUE_LENGTH];
+} ConfigEntry;
+
+int parseConfigFile(const char *filename, ConfigEntry *entries, int *num_entries) {
+    FIL file;
+    FRESULT res;
+    char line[MAX_LINE_LENGTH];
+    int count = 0;
+
+    res = f_open(&file, filename, FA_READ);
+
+    // Read lines from the file
+    while (f_gets(line, sizeof(line), &file)) {
+        // Ignore lines starting with '#' (comments) or empty lines
+        if (line[0] == '#' || line[0] == '\n')
+            continue;
+
+        // Parse the line into key and value
+        char *equal_sign = strchr(line, '=');
+        if (equal_sign != NULL) {
+        	// Replace '=' with null terminator to separate key and value
+            *equal_sign = '\0'; 
+            char *key = line;
+            char *value = equal_sign + 1;
+            
+            // Remove leading/trailing spaces from key and value
+            while (*key == ' ') key++;
+            char *end = value + strlen(value) - 1;
+            while (*end == ' ' || *end == '\n' || *end == '\r') *end-- = '\0';
+            
+            // Copy key and value into the new entry
+            if (strlen(key) < MAX_KEY_LENGTH && strlen(value) < MAX_VALUE_LENGTH) {
+                strncpy(entries[count].key, key, MAX_KEY_LENGTH - 1);
+                strncpy(entries[count].value, value, MAX_VALUE_LENGTH - 1);
+                count++;
+                
+                // Break if maximum number of entries reached
+                if (count >= MAX_ENTRIES) break; 
+            }
+        }
+    }
+
+    *num_entries = count;
+    
+    f_close(&file);
+    
+    return 1;
+}
 
 int main()
 {
 	HS_LEDS(0xFF);
 	
 	BYTE *ldrptr = (BYTE *)0xA00901FC;
+	void (*bin_start)(void) = (void *)0xA0001000;
+	char *file_name[12];
 	
 	FATFS fs0;
 	FIL fil;
 	FRESULT fr;
 	UINT br;
+	ConfigEntry entries[MAX_ENTRIES];
+	int num_entries = 0;
+	int file_size = 0;
+	int i = 0;
 	
 	f_mount(&fs0, "0:", 1);
 	
-	fr = f_open(&fil, "loadusb.bin", FA_READ);
-
-	int sizes = f_size(&fil);
+	// Read config file so that we can set parameters for loading a binary from the USB drive
+	if(parseConfigFile("config.ini", entries, &num_entries)){
+		for(i=0;i<num_entries;++i){
+			if(!strcmp(entries[i].key, "LOAD_ADDR")){
+				ldrptr = (BYTE *)strtoul(entries[i].value, NULL, 16);
+			}
+			if(!strcmp(entries[i].key, "START_ADDR")){
+				bin_start = (void *)strtoul(entries[i].value, NULL, 16);
+			}
+			if(!strcmp(entries[i].key, "LOAD_FILE")){
+				file_name[0] = (char *)entries[i].value;
+			}
+		}	
+	}
 	
-	fr = f_read(&fil, ldrptr, sizes, &br);
+	fr = f_open(&fil, file_name[0], FA_READ);
+
+	file_size = f_size(&fil);
+	
+	fr = f_read(&fil, ldrptr, file_size, &br);
 		
 	f_close(&fil);
+	
+	// Unmount
+    f_mount(NULL, "0:", 0);
 
 	HS_LEDS(0x00);
 	
-	USB_LOAD();	
+	bin_start();	
 
 return 0;		
 }
