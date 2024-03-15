03b36220cedec2ecf5f62c0149d33324814d2df9
hyperscandev <hyperscandev@msbob.org>
2024-03-15 14:42:09 -0400

    Preliminary version of hello world example code
    I have successfully completed the first version of the hello world
    example code.

---
+++ hyperscan/examples/HelloWorld/Debug/Sys_IRQ.d
@@ -0,0 +1,2 @@
+Debug\Sys_IRQ.d Debug\Sys_IRQ.o:  \
+ C:\HyperScan-SPG29x-SDK\hyperscan\scorelibs\IRQ\Sys_IRQ.c
---
+++ hyperscan/examples/HelloWorld/Debug/Sys_isr.d
@@ -0,0 +1,2 @@
+Debug\Sys_isr.d Debug\Sys_isr.o:  \
+ C:\HyperScan-SPG29x-SDK\hyperscan\scorelibs\IRQ\Sys_isr.s
