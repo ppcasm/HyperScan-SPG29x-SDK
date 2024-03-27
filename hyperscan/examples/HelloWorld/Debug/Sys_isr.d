f0f771d331a96216d820474c35bf5a81303416ea
Jonathan Benjamin <ppcasm@gmail.com>
2024-03-27 19:20:25 -0400

    Restructure, refactor

--- hyperscan/examples/HelloWorld/Debug/Sys_isr.d
+++
@@ -1,2 +0,0 @@
-Debug\Sys_isr.d Debug\Sys_isr.o:  \
- C:\HyperScan-SPG29x-SDK\hyperscan\scorelibs\IRQ\Sys_isr.s
---
+++ hyperscan/examples/USB/USBLoader/Debug/libgloss.d
@@ -0,0 +1,8 @@
+Debug\libgloss.d Debug\libgloss.o:  \
+ C:\Users\ppcas\OneDrive\Documents\GitHub\HyperScan-SPG29x-SDK\hyperscan\scorelibs\libgloss.c \
+  C:\Users\ppcas\OneDrive\Documents\GitHub\HyperScan-SPG29x-SDK\hyperscan\scorelibs\/../include/FatFS/ff.h \
+  C:\Users\ppcas\OneDrive\Documents\GitHub\HyperScan-SPG29x-SDK\hyperscan\scorelibs\/../include/FatFS/ffconf.h
+
+C:\Users\ppcas\OneDrive\Documents\GitHub\HyperScan-SPG29x-SDK\hyperscan\scorelibs\/../include/FatFS/ff.h:
+
+C:\Users\ppcas\OneDrive\Documents\GitHub\HyperScan-SPG29x-SDK\hyperscan\scorelibs\/../include/FatFS/ffconf.h:
