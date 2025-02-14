c69dbe480a34f1db1ebeb1b3962499e837403e23
hyperscandev <hyperscandev@gmail.com>
2025-02-14 17:49:17 -0500

    Implemented function for sprite x position by ID
    I've developed a function that retrieves a sprite's x position based on
    its ID.

--- SPG290/scorelibs/PPU/PPU.c
+++ SPG290/scorelibs/PPU/PPU.c
@@ -5,3 +5,8 @@ volatile UV32* getSpritePaletteAddr(unsigned int bank)
 {
 	return P_SPRITE_ATTRIBUTE_BASE + (8 * bank);
 }
+
+volatile int getSpriteXPos(unsigned int id)
+{
+	return P_SPRITE_X_BASE + (8 * id);
+}
